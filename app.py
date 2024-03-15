fee8cc0b156fd3bbeebcbae4e5d93d6f763089ee
samiullah <sami606713@gmail.com>
2024-03-15 11:40:11 +0500

    getting the data and savig the data in the folder

--- app.py
+++ app.py
@@ -2,10 +2,14 @@ from src.SQL_Case_Study.logger import logging
 from src.SQL_Case_Study.exception import CustomException
 import sys
 from src.SQL_Case_Study.utils import get_data
+from src.SQL_Case_Study.components.data_ingestion import DataIngestion
+from src.SQL_Case_Study.components.data_ingestion import DataIngestionConfig
+
 
 if __name__=="__main__":
     logging.info("excution start...")
     try:
-        get_data()
+        data=DataIngestion()
+        data.initisate_data_ingestion()
     except Exception as e:
         raise CustomException(e,sys)
